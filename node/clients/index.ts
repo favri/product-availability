aa8e44870788b33c77877d2d973f3c87373613cf
Tomas A. Mehdi <tomas.mehdi@vtex.com.br>
2021-06-07 16:15:16 -0300

    v0.2.0

--- node/clients/index.ts
+++
@@ -1,9 +0,0 @@
-import { IOClients } from '@vtex/api'
-
-import SkuClient from './skuClient'
-
-export class Clients extends IOClients {
-  public get sku() {
-    return this.getOrSet('skuclient', SkuClient)
-  }
-}
